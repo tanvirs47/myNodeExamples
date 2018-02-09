c7995129ad4bd320e8165576363096a8d00c847d
tanvir <tanvirs47@gmail.com>
2018-02-09 16:12:33 +0530

    second commit1

--- pro1/cryptography.js
+++ pro1/cryptography.js
@@ -3,7 +3,7 @@ var bcrypt = require('bcryptjs');
 
 /* Asynchronous  */
 /*bcrypt.hash('Secret@123',bcrypt.genSaltSync(10),function(err,hash){
-    console.log(hash);
+    //console.log(hash);
 
     bcrypt.compare('Secret@123',hash,function(err,res){
         console.log(res);
